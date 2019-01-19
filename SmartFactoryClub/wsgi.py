309e339d810eac8468508f4f4a5285cd95be9e0e
JiangengDong <dongjiangeng@gmail.com>
2019-01-20 00:26:11 +0800

    modified to work with apache2

--- SmartFactoryClub/wsgi.py
+++ SmartFactoryClub/wsgi.py
@@ -8,6 +8,10 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
+PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+import sys
+sys.path.insert(0, PROJECT_DIR)
 
 from django.core.wsgi import get_wsgi_application
 
