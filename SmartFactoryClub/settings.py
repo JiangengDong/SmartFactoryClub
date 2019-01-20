88e7fc41109ebb574e5095653831d1ed06a45eb1
JiangengDong <dongjiangeng@gmail.com>
2019-01-20 21:45:16 +0800

    fix a problem of language

--- SmartFactoryClub/settings.py
+++ SmartFactoryClub/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'zh-CN'
+LANGUAGE_CODE = 'zh-Hans'
 
 TIME_ZONE = 'Asia/Shanghai'
 
@@ -112,7 +112,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = False
+USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
